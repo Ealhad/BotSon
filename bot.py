1d92cb923552dd212dff3f6389728e4c28750376
ealhad <34lh4d@gmail.com>
2016-07-20 11:06:54 +0200

    chtite amélioration

--- bot.py
+++ bot.py
@@ -1,34 +1,51 @@
-import requests
+# coding=utf-8
+from random import choice
+from requests import get, post
 
 
 def init():
-    """ récupère le cookie de session et le nombre de vidéos dans la file
+    # type: () -> dict, int
+    """ récupère les cookies de session et le nombre de vidéos dans la file
     aucune idée pour l'instant pour le nombre
+    :return: cookies, nombre de vidéos
     """
-    r = requests.get('https://togethertube.com/rooms/salle-de-ballmer')
-    return r.cookies['PLAY_SESSION'], 42  # TODO
+    r = get('https://togethertube.com/rooms/salle-de-ballmer')
+    return {'PLAY_SESSION': r.cookies['PLAY_SESSION']}, 42  # TODO
 
 
-def vote(video):
+def vote(video, cookies):
+    # type: (str, dict) -> bool
     """ vote pour une vidéo
-    https://www.youtube.com/watch?v=<video>
+    :return: succès du vote
+    :param cookies: cookies à ajouter à la requête
+    :param video: id de la vidéo - https://www.youtube.com/watch?v=<vidéo>
     """
-    post = requests.post('https://togethertube.com/api/v1/rooms/salle-de-ballmer/playlist/votes',
-                         json={'mediaId': video, 'mediaServiceId': 'youtube'},
-                         cookies={'PLAY_SESSION': cookie})
-    return post.ok
+    r = post('https://togethertube.com/api/v1/rooms/salle-de-ballmer/playlist/votes',
+             json={'mediaId': video, 'mediaServiceId': 'youtube'},
+             cookies=cookies)
+    return r.ok
 
 
 def get_video():
+    # type: () -> str, str
     """ récupère une vidéo au pif dans videos.list
     (TODO)
+    :return: id, nom de la vidéo
     """
     lines = open('videos.list').read().splitlines()
+    line = choice(lines)
+    pos = line.index(';')
+    return line[:pos].strip(), line[pos + 1:].strip()
 
 
-cookie, number_of_videos = init()
+def main():
+    cookies, _ = init()
+    video, name = get_video()
+    status = vote(video, cookies)
+    if status:
+        print('Vidéo ajoutée : {0} (https://youtube.com/watch?v={1})'.format(name, video))
+    else:
+        print('Erreur !')
 
-if vote('dQw4w9WgXcQ'):
-    print("Vidéo ajoutée !")
-else:
-    print("Snif, snif...")
+
+main()
